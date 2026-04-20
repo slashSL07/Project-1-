8f721968c1cfb8ca94533909213b90e8f843ed8e
Slash <65218031+slashSL07@users.noreply.github.com>
2026-04-20 23:41:59 +0545

    Add game loop and replay functionality
    Refactor game loop to allow replay and improve structure.

--- main.py
+++ main.py
@@ -6,28 +6,32 @@
 
 '''
 import random 
-computer = random.randint(-1, 1)
-key = ["water", "gun", "snake"]
-you = key.index(input("your choice: ")) - 1
-print(f"You choose {key[you + 1]} \n Computer chose {key[computer + 1]}")
+while True:
+    computer = random.randint(-1, 1)
+    key = ["water", "gun", "snake"]
+    you = key.index(input("your choice: ")) - 1
+    print(f"You choose {key[you + 1]} \n Computer chose {key[computer + 1]}")
 
 
 
-if computer == you:
-    print ("Draw")
+    if computer == you:
+        print ("Draw")
 
-else:
-
-    if(computer==-1 and you == 1):
-        print("You win")    
-    elif (computer==-1 and you==0):
-        print("You Win")
-    elif (computer==0 and you==-1):
-        print("You Win")
-    elif (computer==1 and you==0):
-        print("You Win")
     else:
-        print("You loose")
+
+        if(computer==-1 and you == 1):
+            print("You win")    
+        elif (computer==-1 and you==0):
+            print("You Win")
+        elif (computer==0 and you==-1):
+            print("You Win")
+        elif (computer==1 and you==0):
+            print("You Win")
+        else:
+            print("You loose")
+    c = print("want to play again(y/n): ")
+    if c == "n":
+        break
 
          # We can replace the ladder by the following logic ..   
 
